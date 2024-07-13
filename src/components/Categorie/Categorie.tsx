506d09bc23ed4ad3c799ae3d26b540dec3e461f0
Kamrul Hassan <kamrulthedev@gmail.com>
2024-07-13 11:27:53 +0600

    complate-categorie-section-commit

--- src/components/Categorie/Categorie.tsx
+++ src/components/Categorie/Categorie.tsx
@@ -4,25 +4,25 @@ const categories = [
   {
     name: "Cardio",
     image:
-      "https://i.ibb.co/qyKwgfP/shirtless-bodybuilder-doing-side-plank-exercise-7502-4749.jpg",
+      "https://i.ibb.co/nRBv8dD/young-woman-doing-fitness-sportswear.jpg",
     route: "/products?category=cardio",
   },
   {
     name: "Strength",
     image:
-      "https://i.ibb.co/qyKwgfP/shirtless-bodybuilder-doing-side-plank-exercise-7502-4749.jpg",
+      "https://i.ibb.co/KG5pxx4/studio-portrait-bearded-brutal-male-isolated-gray-background.jpg",
     route: "/products?category=strength",
   },
   {
     name: "Accessories",
     image:
-      "https://i.ibb.co/qyKwgfP/shirtless-bodybuilder-doing-side-plank-exercise-7502-4749.jpg",
+      "https://i.ibb.co/MNkXMj1/powerful-stylish-bodybuilder-with-tattoo-his-arm-doing-exercises-with-dumbbells-isolated-dark-backgr.jpg",
     route: "/products?category=accessories",
   },
   {
     name: "Outlet",
     image:
-      "https://i.ibb.co/qyKwgfP/shirtless-bodybuilder-doing-side-plank-exercise-7502-4749.jpg",
+      "https://i.ibb.co/T1wp3qt/female-athlete-exercising-with-kettlebell-798657-9840.jpg",
     route: "/products?category=outlet",
   },
   {
@@ -47,8 +47,8 @@ const Categorie = () => {
   };
 
   return (
-    <div className="categories-section p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-      {categories?.slice(0, 3).map((category) => (
+    <div className="categories-section py-16 px-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
+      {categories?.slice(0, 4).map((category) => (
         <button
           key={category.name}
           onClick={() => handleCategoryClick(category.route)}
@@ -56,8 +56,9 @@ const Categorie = () => {
           <img
             src={category.image}
             alt={category.name}
-            className="w-full hover:p-2 h-48 object-cover  rounded-lg mb-4"
+            className="w-72 hover:p-2 h-72 object-cover  rounded-lg mb-2"
           />
+          <p className="flex justify-start text-2xl font-serif uppercase">{category.name}</p>
         </button>
       ))}
     </div>
