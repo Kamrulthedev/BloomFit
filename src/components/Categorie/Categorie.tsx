f74af98fd72ec321e5693dd7d088771a530ea85f
Kamrul Hassan <kamrulthedev@gmail.com>
2024-07-13 16:14:01 +0600

    update-categorie

--- src/components/Categorie/Categorie.tsx
+++ src/components/Categorie/Categorie.tsx
@@ -47,7 +47,7 @@ const Categorie = () => {
   };
 
   return (
-    <div className="categories-section py-16 px-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
+    <div className="categories-section py-16 px-8 grid grid-cols-2 lg:grid-cols-4 gap-6 ">
       {categories?.slice(0, 4).map((category) => (
         <button
           key={category.name}
@@ -56,7 +56,7 @@ const Categorie = () => {
           <img
             src={category.image}
             alt={category.name}
-            className="w-72 hover:p-2 h-72 object-cover  rounded-lg mb-2"
+            className="w-72 hover:p-2 h-52 lg:h-72 object-cover  rounded-lg mb-2"
           />
           <p className="flex justify-start text-2xl font-serif uppercase">{category.name}</p>
         </button>
