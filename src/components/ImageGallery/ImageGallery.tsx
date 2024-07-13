7ddad03efc9b0f1f8dead7d5a58090d7e22d5339
Kamrul Hassan <kamrulthedev@gmail.com>
2024-07-13 18:13:32 +0600

    animation use

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,3 +1,6 @@
+import { animated,useSpring } from "@react-spring/web";
+import { useEffect, useState } from "react";
+
 const images = [
   "https://i.ibb.co/fk844qP/john-arano-h4i9-G-de7-Po-unsplash.jpg",
   "https://i.ibb.co/f4MB2KF/sam-sabourin-Pi-FA6-HIAf-BA-unsplash.jpg",
@@ -14,34 +17,45 @@ const images1 = [
 ];
 
 const ImageGallery = () => {
+  const [visible, setVisible] = useState(false);
+
+  const props = useSpring({
+    opacity: visible ? 12 : 8,
+    transform: visible ? 'translateY(0)' : 'translateY(20px)',
+    from: { opacity: 0, transform: 'translateY(20px)' },
+  });
+
+  useEffect(() => {
+    setVisible(true);
+  }, []);
   return (
-    <div className="image-gallery p-4 sm:p-8 bg-gray-100">
-    <h2 className="text-xl lg:text-3xl font-serif text-center mb-4 lg:mb-8 uppercase">
-      Gallery
-    </h2>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4 mb-4">
-      {images1.slice(0, 3).map((image, index) => (
-        <div key={index} className="relative overflow-hidden h-48 lg:h-96">
-          <img loading="lazy"
-            src={image}
-            alt={`Gallery Image ${index + 1}`}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      ))}
-    </div>
-    <div className="grid md:grid-cols-4 sm:grid-cols-4 lg:grid-cols-5 gap-2 lg:gap-4">
-      {images.map((image, index) => (
-        <div key={index} className="relative overflow-hidden h-32 sm:h-48 lg:h-60">
-          <img loading="lazy"
-            src={image}
-            alt={`Gallery Image ${index + 1}`}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      ))}
-    </div>
-  </div>
+    <animated.div style={props} className="image-gallery p-8 bg-gray-100 md:h-[750px]">
+      <h2 className="lg:text-3xl font-serif text-center mb-8 uppercase">Gallery</h2>
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 lg:h-96 mb-2">
+        {images1.slice(0, 3).map((image, index) => (
+          <div key={index} className="relative overflow-hidden">
+            <img
+              src={image}
+              alt={`Gallery Image ${index + 1}`}
+              className="w-full h-full object-cover"
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-4 sm:grid-cols-2 lg:grid-cols-5 gap-2">
+        {images.map((image, index) => (
+          <div key={index} className="relative overflow-hidden">
+            <img
+              src={image}
+              alt={`Gallery Image ${index + 1}`}
+              className="w-full h-full object-cover"
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
+    </animated.div>
   );
 };
 
