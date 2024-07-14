78fa539098d7f6ac572910bc93bcf1a4f36b94c6
Kamrul Hassan <kamrulthedev@gmail.com>
2024-07-14 09:15:31 +0600

    update-commit

--- src/components/ShopAll/ProductCard.tsx
+++ src/components/ShopAll/ProductCard.tsx
@@ -1,22 +1,33 @@
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white">
-      <img className="w-full object-cover transition-transform hover:scale-x-110" src={product.image} alt={'Images'} />
-      <div className="px-6 py-4">
+    <div className="rounded-lg overflow-hidden shadow-lg p-2 bg-white">
+      <img
+        className="w-full object-cover p-4 transition-transform hover:scale-x-110"
+        src={product.image}
+        alt={"Images"}
+      />
+      <div className="px-6 py-4 space-y-2">
         <div className="font-bold text-xl mb-2">{product.name}</div>
         <div className="flex items-center">
-          {Array.from({ length: 5 }, (_, index) => (
-            index 
-              ? <AiFillStar key={index} className="text-yellow-400" />
-              : <AiOutlineStar key={index} className="text-yellow-400" />
-          ))}
+          {Array.from({ length: 8 }, (_, index) =>
+            index ? (
+              <AiOutlineStar key={index} className="text-yellow-400" />
+            ) : (
+              <AiFillStar key={index} className="text-yellow-400" />
+            )
+          )}
           <span className="text-gray-600 ml-2">({product.reviews})</span>
         </div>
-        <p className="text-gray-700 text-base mt-2">From ${product.price.toFixed(2)}</p>
-        <p className="text-gray-500 text-sm mt-1">{product.financeMessage} <a href={product.financeLink} className="text-blue-500 underline">See if you qualify</a></p>
+        <p className="text-gray-700 text-base">
+          From ${product.price.toFixed(2)}
+        </p>
       </div>
+      <Link className="p-2" to="/">
+          view details
+        </Link>
     </div>
   );
 };
