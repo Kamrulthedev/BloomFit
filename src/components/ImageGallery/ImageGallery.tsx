4b30f080c9ca1796fdaee26dab33bb174356be57
Kamrul Hassan <kamrulthedev@gmail.com>
2024-07-30 07:43:50 +0600

    update-gallary-commit

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,12 +1,9 @@
-import { animated,useSpring } from "@react-spring/web";
+import { animated, useSpring } from "@react-spring/web";
 import { useEffect, useState } from "react";
 
 const images = [
   "https://i.ibb.co/fk844qP/john-arano-h4i9-G-de7-Po-unsplash.jpg",
   "https://i.ibb.co/f4MB2KF/sam-sabourin-Pi-FA6-HIAf-BA-unsplash.jpg",
-  "https://i.ibb.co/341qpJr/toni-g-6-T-6h3-Z39-Ls-unsplash.jpg",
-  "https://i.ibb.co/1G6493b/Screenshot-2024-07-13-145315.png",
-  "https://i.ibb.co/Zmdh1Xr/daniel-apodaca-Wdo-Qio6-HPVA-unsplash.jpg",
 ];
 
 const images1 = [
@@ -14,6 +11,8 @@ const images1 = [
   "https://i.ibb.co/VBdGsDF/Screenshot-2024-07-13-144505.png",
   "https://i.ibb.co/cy5c0s4/Screenshot-2024-07-13-143736.png",
   "https://i.ibb.co/cy5c0s4/Screenshot-2024-07-13-143736.png",
+  "https://i.ibb.co/cy5c0s4/Screenshot-2024-07-13-143736.png",
+  "https://i.ibb.co/Yhf8Rg5/Screenshot-2024-07-13-145608.png",
 ];
 
 const ImageGallery = () => {
@@ -21,8 +20,8 @@ const ImageGallery = () => {
 
   const props = useSpring({
     opacity: visible ? 12 : 8,
-    transform: visible ? 'translateY(0)' : 'translateY(20px)',
-    from: { opacity: 0, transform: 'translateY(20px)' },
+    transform: visible ? "translateY(0)" : "translateY(20px)",
+    from: { opacity: 0, transform: "translateY(20px)" },
   });
 
   useEffect(() => {
@@ -30,30 +29,57 @@ const ImageGallery = () => {
   }, []);
   return (
     <animated.div style={props} className="image-gallery p-8 bg-gray-100">
-      <h2 className="lg:text-3xl font-serif text-center mb-8 uppercase">Gallery</h2>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:h-64 lg:h-96 mb-2">
-        {images1.slice(0, 3).map((image, index) => (
-          <div key={index} className="relative md:overflow-auto lg:overflow-hidden">
-            <img
-              src={image}
-              alt={`Gallery Image ${index + 1}`}
-              className="w-full h-full object-cover hover:scale-x-110 transition-transform"
-              loading="lazy"
-            />
+      <h2 className="lg:text-3xl font-serif text-center mb-8 uppercase">
+        Gallery
+      </h2>
+
+      <div className="grid grid-cols-2 gap-2">
+        {/* first div */}
+        <div>
+          {images1.slice(2, 3).map((image, index) => (
+            <div
+              key={index}
+              className="relative md:overflow-auto lg:overflow-hidden"
+            >
+              <img
+                src={image}
+                alt={`Gallery Image ${index + 1}`}
+                className="w-full  object-cover hover:scale-x-110 transition-transform"
+                loading="lazy"
+              />
+            </div>
+          ))}
+        </div>
+        {/* second div */}
+        <div>
+          <div className="grid grid-cols-2 lg:grid-cols-2 gap-4 mb-2">
+            {images1.slice(0, 2).map((image, index) => (
+              <div
+                key={index}
+                className="relative md:overflow-auto lg:overflow-hidden"
+              >
+                <img
+                  src={image}
+                  alt={`Gallery Image ${index + 1}`}
+                  className="w-full h-full object-cover hover:scale-x-110 transition-transform"
+                  loading="lazy"
+                />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="grid md:grid-cols-4 sm:grid-cols-2 lg:grid-cols-5 gap-2 md:h-48 lg:h-72">
-        {images.map((image, index) => (
-          <div key={index} className="relative overflow-hidden">
-            <img
-              src={image}
-              alt={`Gallery Image ${index + 1}`}
-              className="w-full h-full object-cover hover:scale-x-105 transition-transform"
-              loading="lazy"
-            />
+          <div className="grid md:grid-cols-2 grid-cols-2 lg:grid-cols-2 gap-2">
+            {images.map((image, index) => (
+              <div key={index} className="relative overflow-hidden">
+                <img
+                  src={image}
+                  alt={`Gallery Image ${index + 1}`}
+                  className="w-full h-full object-cover hover:scale-x-105 transition-transform"
+                  loading="lazy"
+                />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </animated.div>
   );
